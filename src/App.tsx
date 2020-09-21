db8b850ea2c0a71b6c6f2e2267a266603ea3e310
Cesar <cesar@cssguy4hire.com>
2020-09-21 21:16:52 +0300

    001: Initial commit -  Detached from Create React App

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface Props {
+  test: string;
+}
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Movie Library</h1>
+      </header>
+      <main>
+        Main content here
+        </main>
+      <footer>
+        Footer here
+      </footer>
+    </>
+  );
+}
+
+export default App;

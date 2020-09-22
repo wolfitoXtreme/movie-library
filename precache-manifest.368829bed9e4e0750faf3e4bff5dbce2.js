78a2b35ea09618c923cb535d180cae5f31c5db0f
Cesar <cesar@cssguy4hire.com>
2020-09-22 19:11:04 +0300

    Updates

---
+++ precache-manifest.368829bed9e4e0750faf3e4bff5dbce2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ed8da88aa790be78a78e943b9036c5",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "c7d10b5ee60eb4627a48",
+    "url": "/movie-library/static/css/2.5e188a09.chunk.css"
+  },
+  {
+    "revision": "7ea83cf68aee5d4cd37a",
+    "url": "/movie-library/static/css/main.9d4f164b.chunk.css"
+  },
+  {
+    "revision": "c7d10b5ee60eb4627a48",
+    "url": "/movie-library/static/js/2.682f57e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-library/static/js/2.682f57e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea83cf68aee5d4cd37a",
+    "url": "/movie-library/static/js/main.7f4e01f9.chunk.js"
+  },
+  {
+    "revision": "013c70500261c060a513",
+    "url": "/movie-library/static/js/runtime-main.cba2209a.js"
+  },
+  {
+    "revision": "2aef3b447398d8aed85fe3790154d18a",
+    "url": "/movie-library/static/media/anton-regular.2aef3b44.ttf"
+  },
+  {
+    "revision": "7df7d945f3c6a2f96fc00e3149bb30a3",
+    "url": "/movie-library/static/media/anton-regular.7df7d945.woff"
+  },
+  {
+    "revision": "eb497bceb1cbaf300d5eaa3c70d6ff94",
+    "url": "/movie-library/static/media/anton-regular.eb497bce.woff2"
+  },
+  {
+    "revision": "1f2908fc1774e1ff4947fb6c0d10dced",
+    "url": "/movie-library/static/media/opensans-bold.1f2908fc.woff2"
+  },
+  {
+    "revision": "2c11176abb31d7de232be16caf71de14",
+    "url": "/movie-library/static/media/opensans-bold.2c11176a.woff"
+  },
+  {
+    "revision": "382d9ed1470814d72e17abd2e51d0ffc",
+    "url": "/movie-library/static/media/opensans-bold.382d9ed1.ttf"
+  },
+  {
+    "revision": "77b2864f64ff03c3ee5ba41d4254dc29",
+    "url": "/movie-library/static/media/opensans-extrabold.77b2864f.ttf"
+  },
+  {
+    "revision": "ae99dc44eb0255d04410084e9479d9cf",
+    "url": "/movie-library/static/media/opensans-extrabold.ae99dc44.woff2"
+  },
+  {
+    "revision": "dc966f35e9371e67ccc112e416190f67",
+    "url": "/movie-library/static/media/opensans-extrabold.dc966f35.woff"
+  },
+  {
+    "revision": "49ad2a4a8644191118b2e5fe23942e9d",
+    "url": "/movie-library/static/media/opensans-light.49ad2a4a.ttf"
+  },
+  {
+    "revision": "93a9ddfdfe51c15e86f7ea2e0511cbda",
+    "url": "/movie-library/static/media/opensans-light.93a9ddfd.woff"
+  },
+  {
+    "revision": "ceb56c487486441dea908857f87ffffd",
+    "url": "/movie-library/static/media/opensans-light.ceb56c48.woff2"
+  },
+  {
+    "revision": "2374f019ae14ad70b503dbaaeaddb67e",
+    "url": "/movie-library/static/media/opensans-regular.2374f019.woff2"
+  },
+  {
+    "revision": "d04c61868e6f31b9cfa1c9e7f3d65b1e",
+    "url": "/movie-library/static/media/opensans-regular.d04c6186.woff"
+  },
+  {
+    "revision": "d8fc6646796652331030772dc31f0f8f",
+    "url": "/movie-library/static/media/opensans-regular.d8fc6646.ttf"
+  },
+  {
+    "revision": "656cfcc0b20f01002721dc7220057e04",
+    "url": "/movie-library/static/media/opensans-semibold.656cfcc0.woff2"
+  },
+  {
+    "revision": "85962b6a873aa9628fd03f622bb38833",
+    "url": "/movie-library/static/media/opensans-semibold.85962b6a.woff"
+  },
+  {
+    "revision": "c938d4e7fc36b87f5ee8acb64de49f70",
+    "url": "/movie-library/static/media/opensans-semibold.c938d4e7.ttf"
+  }
+]);
